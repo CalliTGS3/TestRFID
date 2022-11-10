52fbd6d1cc4024d40a6129c2dbbb2e9ef07daa96
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2022-11-10 16:49:17 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    // Write data to card
+    MFRC522.write("Raik")
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    leseID = !(leseID)
+    if (leseID) {
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.setLedColor(0xff0000)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    data = MFRC522.read()
+    serial.writeLine("DATA:" + data)
+})
+let data = ""
+let leseID = false
 serial.redirectToUSB()
+serial.writeLine("Start")
 // Initialize RFID module
 MFRC522.Init(
 DigitalPin.C9,
@@ -6,7 +25,16 @@ DigitalPin.C8,
 DigitalPin.C7,
 DigitalPin.P0
 )
+serial.writeLine("Initialisiert")
+leseID = true
+if (leseID) {
+    basic.setLedColor(0x00ff00)
+} else {
+    basic.setLedColor(0xff0000)
+}
 basic.forever(function () {
-    // Get ID of RFID card
-    serial.writeLine("" + (MFRC522.getID().toString()))
+    if (leseID) {
+        let id = MFRC522.getID().toString()
+serial.writeLine("ID:" + id)
+    }
 })
